b00ce58a04761a1dbe83be0ea80ee1c61d89e81f
georgetarazevich <tarayezh@gmail.com>
2017-03-09 22:45:52 +0300

    Unit_Pen_Testing first commit
    Unit_Pen_Testing first commit

---
+++ src/test/java/com/unit/pen/testing/TestBase.java
@@ -0,0 +1,48 @@
+package com.unit.pen.testing;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+
+import com.beust.jcommander.Parameter;
+import com.unit.pen.testing.Pen;
+
+public class TestBase {
+
+	private final int inkContainerValue = 500;
+	private final int inkContainerZeroValue = 0;
+	private final double sizeLetterValue = 2.0;
+	private final double sizeLetterZeroValue = 0.0;
+	private final String color = "BLUE";
+
+	@DataProvider(name = "testingInkContainer_TrueValue")
+	public Object[][] createPens_InkContainer_TrueValue() {
+		return new Object[][] {
+				new Object[] { new Pen(inkContainerValue) },
+				new Object[] { new Pen(inkContainerValue, sizeLetterValue) },
+				new Object[] { new Pen(inkContainerValue, sizeLetterValue, color) } };
+	}
+
+	@DataProvider(name = "testingInkContainer_FalseValue")
+	public Object[][] createPens_InkContainer_FalseValue() {
+		return new Object[][] {
+				new Object[] { new Pen(inkContainerZeroValue) },
+				new Object[] { new Pen(inkContainerZeroValue, sizeLetterValue) },
+				new Object[] { new Pen(inkContainerZeroValue, sizeLetterValue, color) },
+				new Object[] { new Pen(inkContainerZeroValue - 1) },
+				new Object[] { new Pen(inkContainerZeroValue - 1, sizeLetterValue) },
+				new Object[] { new Pen(inkContainerZeroValue - 1, sizeLetterValue, color) } };
+	}
+
+	@DataProvider(name = "testingSizeLetter_FalseValue")
+	public Object[][] createPens_SizeLetter_FalseValue() {
+		return new Object[][] {
+				new Object[] { new Pen(inkContainerValue, sizeLetterZeroValue) },
+				new Object[] { new Pen(inkContainerValue, sizeLetterZeroValue - 1, color) } };
+	}
+
+	@BeforeMethod
+	public void setup() {
+
+	}
+}
